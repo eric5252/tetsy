acaaab2aab05643dbc724f321c01abff262c9abd
Eric Hsieh <525chungli@gmail.com>
2020-04-29 14:06:24 -0700

    fix

---
+++ frontend/tetsy.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+  const store = configureStore()
+  
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>ETSY</h1>, root);
+});
